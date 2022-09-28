6d79836f4ae392fcee91ef8ad6f910d4d2d34577
sh_rn <shrnkld@pm.me>
2022-09-28 08:54:04 +0200

    add new site dir for static output

---
+++ site/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+	// adds target=_blank to https and pdf links so they open in a new tab
+	var links = document.getElementsByTagName('a');
+	for (var i = 0; i < links.length; i++) {
+		// open external links and pdfs in new tab
+		if (/^(https?:)?\/\/|(\.pdf)/.test(links[i].getAttribute('href'))) {
+			links[i].target = '_blank';
+		}
+		// open docs index in same tab
+		if (/^(https?:\/\/docs\.blocknet\.co\/?)$/.test(links[i].getAttribute('href'))) {
+			links[i].target = '_self';
+		}
+	}
+});
